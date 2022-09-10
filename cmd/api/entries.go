8c5b9dc17202a71ca03d1c721a5fb2e61a0ddbf8
jadah <2018118161@ub.edu.bz>
2022-09-10 13:06:25 -0700

    Moved the code that checks for the id parameter into its own method readIDParam

--- cmd/api/entries.go
+++ cmd/api/entries.go
@@ -4,9 +4,9 @@ package main
 import	(
 	"fmt"
 	"net/http"
-	"strconv"
+	//"strconv"
 
-	"github.com/julienschmidt/httprouter"
+	//"github.com/julienschmidt/httprouter"
 )
 //create entry handler for the POST /v1/entries endpoint
 func (app *application) createEntryHandler (w http.ResponseWriter, r *http.Request){
@@ -14,11 +14,8 @@ func (app *application) createEntryHandler (w http.ResponseWriter, r *http.Reque
 }
 //create entry handler for the GET /v1/entries/:id endpoint
 func (app *application) showEntryHandler (w http.ResponseWriter, r *http.Request){
-	//use the '*ParamsfromContext())' function to get the request context as a slice
-	params := httprouter.ParamsFromContext(r.Context())
-	//get the value of the 'id' parameter
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1 {
+	id, err := app.readIDParam(r)
+	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
