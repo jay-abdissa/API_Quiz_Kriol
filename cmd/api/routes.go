fdb24e2ca78af8c49d0e0ad2689cdf79a7e87987
jadah <2018118161@ub.edu.bz>
2022-09-10 10:50:32 -0700

    testing commit

---
+++ cmd/api/routes.go
@@ -0,0 +1,17 @@
+//Filename:cmd/api/routes.go
+package main
+
+import	(
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+func (app *application) routes() *httprouter.Router{
+	//Create new httprouter instance
+	router := httprouter.New()
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	//router.HandlerFunc(http.MethodPost, "/v1/entries", app.createEntryHandler)
+	//router.HandlerFunc(http.MethodGet, "/v1/entries/:id", app.showEntryHandler)
+	return router
+}
+
